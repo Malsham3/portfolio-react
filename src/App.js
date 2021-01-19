ebfce2a2e2fc604381dbcd69305f7924f6590d5f
Mostafa <m.alsham627@gmail.com>
2021-01-18 22:50:36 -0700

    App file setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { BrowserRouter, Route } from "react-router-dom";
+import Contact from "./pages/Contact";
+import Work from "./pages/Work";
+import Home from "./pages/Home";
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <BrowserRouter>
+      <Navigation />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/contact" component={Contact} />
+      <Route exact path="/work" component={Work} />
+    </BrowserRouter>
   );
 }
-
-export default App;
