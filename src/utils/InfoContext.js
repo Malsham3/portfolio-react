6d57bad30135554d743df8b0d150b56b573e48ba
Mostafa <m.alsham627@gmail.com>
2021-03-18 14:15:48 -0700

    new linkedIn link

--- src/utils/InfoContext.js
+++ src/utils/InfoContext.js
@@ -5,7 +5,7 @@ const InfoContext = React.createContext({
   phoneNumber: "(623)-363-5283",
   email: "m.alsham627@gmail.com",
   gitHub: "https://github.com/Malsham3",
-  linkedIn: "https://www.linkedin.com/in/mostafa-alshammary-22698a1b9/",
+  linkedIn: "https://www.linkedin.com/in/mostafa-alshammary/",
   bio1: `Music enthusiast, fitness  and health fanatic, traveler and more. Fueld by high energy and passion. A quick learner, who's able to pick up new skills and take on different projects with relative ease`,
   bio2: `Loves to design, build clean and user-friendly web applications. Also enjoys finding bugs and fixing broken code.`
   
